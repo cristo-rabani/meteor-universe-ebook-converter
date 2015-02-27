3cd168baccb3b650f767c58aab8b24f07828b0dc
cristo-rabani <cristo.rabani@gmail.com>
2015-02-27 21:26:03 +0100

    github

---
+++ README.md
@@ -0,0 +1,49 @@
+# Universe eBook Converter is wrapper around command-line tool of Calibre
+Convert an ebook from one format to another
+This plugin is dedicated to universe environment but it can work as regular meteor plugin.
+
+## Installation:
+
+To install or upgrade, simply copy paste the following command into a terminal and press Enter:
+
+```
+sudo -v && wget -nv -O- https://raw.githubusercontent.com/kovidgoyal/calibre/master/setup/linux-installer.py | sudo python -c "import sys; main=lambda:sys.stderr.write('Download failed\n'); exec(sys.stdin.read()); main()"
+```
+
+## How To Use:
+
+```
+UniEBookConverter.doConvert({
+    'source': 'testov.html',
+    'target': 'testo.pdf',
+    //optionals
+    arguments: [
+        ['base-font-size', '12'],
+        ['extra-css', 'test.css']
+      ]
+}, function(err, res){ //callback called on the end of process, where res = {log, errorLog} || null
+    console.log(err, res);
+});
+```
+
+
+The default directory for source and target is '/tmp/'
+but you can change it using those methods:
+
+```
+UniEBookConverter.setSourceDirectory('/home/cristo/tmp/');
+UniEBookConverter.setTargetDirectory('/home/cristo/tmp/');
+```
+If you want specify default options, you can do it using:
+
+```
+UniEBookConverter.setDefaultArguments([['base-font-size', '12'], ['extra-css', 'test.css']]);
+```
+
+## Options
+Options depend on calibre version, details are here: http://manual.calibre-ebook.com/cli/ebook-convert.html
+Each option we pass as 2 elements array [name, value], where name of option is without prefix '--'
+
+## License:
+MIT
+
---
+++ ebookConverter.js
@@ -0,0 +1,75 @@
+'use strict';
+
+var spawn = Npm.require('child_process').spawn;
+
+//var re = /(?:\.([^.]+))?$/;
+var _sourceDir = '/tmp/', _targetDir = '/tmp/', _defaultParams = [];
+/* global UniEBookConverter: true */
+UniEBookConverter = {
+    setSourceDirectory: function(path){
+        _sourceDir = path;
+    },
+    setTargetDirectory: function(path){
+        _targetDir = path;
+    },
+    setDefaultArguments: function(params){
+        _defaultParams = params;
+    },
+    doConvert: function(options, cb){
+        if(!_.isFunction(cb)){
+            cb = function(err){
+              if(err){
+                  console.error(err);
+              }
+            };
+        }
+        var callback = _.once(cb);
+        if(!_.isObject(options)){
+            throw new Meteor.Error('Options must be an object with keys: "source", "target"');
+        }
+        var source = _sourceDir + options.source;
+        var target = _targetDir + options.target;
+        var params = [source, target];
+        params = params.concat(_defaultParams);
+        if (UniUtils.get(options, 'arguments.length')) {
+            params = params.concat(options.arguments)
+        }
+        //var targetType = re.exec(target);
+        var _log = '';
+        var _errorLog = '';
+
+        var convert = spawn('ebook-convert', params);
+        convert.stdout.setEncoding('utf8');
+        convert.stdout.on('data', function(data){
+            _log += data.toString();
+        });
+        convert.stderr.on('data', function(err){
+            _errorLog += err.toString();
+        });
+        convert.on('error', function(err){
+            callback(err, {log: _log, errorLog: _errorLog});
+        });
+        convert.on('exit', function(code){
+            callback(code, {log: _log, errorLog: _errorLog});
+        });
+
+    }
+};
+
+//Test if we have installed ebook-convert...
+var _v = '';
+var _s = spawn('ebook-convert', ['--version']).on('error', function(err){
+    if(err){
+        console.error('Do you have calibre ebook-convert installed? Please visit http://calibre-ebook.com/download_linux', err);
+    }
+});
+_s.stdout.on('data', function(data){
+    _v += data.toString();
+});
+_s.on('exit', function(){
+    _v = /[0-9]+.[0-9]+.[0-9]+/.exec(_v);
+    if(_.isArray(_v)){
+        _v = _v.join();
+    }
+    console.log('=> Calibre found in version: '+ (_v || 'unknown'));
+});
