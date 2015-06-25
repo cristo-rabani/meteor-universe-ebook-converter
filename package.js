98247f3d4c193cd66addf4da3ce74b93fa0d26a0
cristo-rabani <cristo.rabani@gmail.com>
2015-06-25 15:44:25 +0200

    up

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
     name: 'vazco:universe-ebook-converter',
     summary: 'Convert an ebook from one format to another. Supported eg. files kindle, pdf, azw3, epub, mobi, docx',
-    version: '1.0.2',
+    version: '1.0.3',
     git: 'https://github.com/cristo-rabani/universe-ebook-converter.git'
 });
 
@@ -9,7 +9,7 @@ Package.onUse(function (api) {
     api.versionsFrom(['METEOR@1.0']);
     api.use([
         'underscore',
-        'vazco:universe-utilities@1.0.2'
+        'vazco:universe-utilities@1.1.6'
     ], ['server']);
     api.add_files('ebookConverter.js', 'server');
     api.export(['UniEBookConverter']);
